806635e65a2e727e780413357245cda0f7b55da9
Logan <loganvt8@vt.edu>
2018-08-26 20:05:28 +0000

    Calls classes to build database and get sprites

---
+++ Java/Runner.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Runner
+{
+    public static void main(String[] str) throws IOException
+    {
+		fixer fix = new fixer();
+        MoveWriter move = new MoveWriter();
+    }
+}
